79ec91e566e1100ded330153861d94a4420a7285
tssl2002 <edcagi@live.com>
2015-07-12 10:58:19 +0200

    Creacion del Modelo y Base de datos

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -1,10 +1,20 @@
+
+var models = require('../models/models.js')
+
 exports.question = function(req,res) {
-	res.render('quizes/question', { pregunta : "¿Cual es la capital de Italia?"});
-};
+	models.Quiz.findAll().then( function(quiz) {
+		res.render('quizes/question', { pregunta : quiz[0].pregunta});
+	});
+}	
 
 exports.answer = function(req,res) {
-	
-	if (req.query.respuesta === "Roma") { 
+	models.Quiz.findAll().then( function(quiz) {
+	if (req.query.respuesta === quiz[0].respuesta) { 
 			res.render('quizes/answer', { resultado : "correcto", enlace : '../', mensaje : "Volver a la pagina principal"});}
 	else {res.render('quizes/answer', { resultado : "incorrecto", enlace : '../quizes/question', mensaje : "Volver a intentarlo >>" });}
+	});
 };
+
+
+
+
