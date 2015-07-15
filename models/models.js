d18bf97d760c49af5136084d4c3e8fc4f8d62147
tssl2002 <edcagi@live.com>
2015-07-15 15:49:07 +0200

    Despliege de la BD en Heroku

--- models/models.js
+++ models/models.js
@@ -4,7 +4,26 @@
 var path = require('path');
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize(null,null,null,{ dialect : "sqlite", storage : "quiz.sqlite"});
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
+
+var sequelize = new Sequelize(DB_name, user, pwd, 
+  { dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  
+    omitNull: true      
+  }      
+);
 
 
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
