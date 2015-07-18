3fbf51c2ba7776d77667eb33898e4e71c64fcfab
tssl2002 <edcagi@live.com>
2015-07-18 07:45:42 +0200

    Autoload y correccion de errores

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -2,27 +2,33 @@
 var models = require('../models/models.js')
 var cadena = require('../functions/cadenas.js')
 
+exports.load = function(req,res,next,quizId) {
+	models.Quiz.findById(quizId).then(
+		function (quiz) {
+			if (quiz) {
+				req.quiz = quiz;
+				next();
+			} else { next( new Error('No existe la pregunta ' + quizId))}
+		}).catch(function (error) { next(error);});
+}
+
 exports.index = function(req,res) {
 	models.Quiz.findAll().then(function(quizes){
 			res.render('quizes/index.ejs',{ quizes : quizes });
-	});
+	}).catch(function (error) { next(error);});
 }
 
 exports.show = function(req,res) {
-	models.Quiz.findById(req.params.quizId).then(function(quiz){
-			res.render('quizes/show',{ quiz : quiz });
-	});
+			res.render('quizes/show',{ quiz : req.quiz });
 }
 
 exports.answer = function(req,res) {
-	models.Quiz.findById(req.params.quizId).then(function(quiz) {
 		var resp = cadena.textoPlano(req.query.respuesta);
-		var solucion = cadena.textoPlano(quiz.respuesta);
+		var solucion = cadena.textoPlano(req.quiz.respuesta);
 		if ( resp === solucion ) {
 			res.render('quizes/answer',{ resultado : "correcto", enlace : '../', mensaje : "Volver a la pagina principal"});
 	} else {res.render('quizes/answer', { resultado : "incorrecto", enlace : '../'+quiz.id, mensaje : "Volver a intentarlo >>" });}
 	
-	});
 }
 
 
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.param('quizId', quizController.load);
+
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer)
