e9be7b2f6aa9f09db05a7fd62ea9296ce531cc72
tssl2002 <edcagi@live.com>
2015-07-19 10:31:20 +0200

    Añadir preguntas

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -39,5 +39,19 @@ exports.answer = function(req,res) {
 	
 }
 
+exports.new = function (req,res) {
+	var quiz = models.Quiz.build({ pregunta:'Pregunta',respuesta:'Respuesta'});
+	res.render('quizes/new',{ quiz : quiz});
+}
+
+exports.create = function(req,res) {
+	var quiz = models.Quiz.build(req.body.quiz);
+
+	quiz.save({fields: ["pregunta","respuesta"]}).then(function() {
+		res.redirect('/quizes');
+		
+	});
+}
+
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quizController');
-/* GET home page. */
+
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
 router.param('quizId', quizController.load);
 
-router.get('/quizes', quizController.index);
-router.get('/quizes/:quizId(\\d+)', quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer',quizController.answer)
+router.get('/quizes', 							quizController.index);
+router.get('/quizes/:quizId(\\d+)', 			quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',		quizController.answer);
+router.get('/quizes/new',						quizController.new);
+router.post('/quizes/create',					quizController.create);
 
 router.get('/author', function(req,res) {
 	res.render('author');
